b41e6f2ad764ee42378f13416d1983249d7b9524
philipmo <philipmo59@gmail.com>
2023-09-07 15:22:51 -0400

    finished

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,61 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+    try: 
+        user_password = int(password) 
+    except ValueError: 
+        return "Access denied"
+    if username.lower() == "admin" and user_password == 12345:
+        return 'Access granted'
+    else:
+        return 'Access denied'  
+    
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature == 33:
+        return "It's brisk out there!"
+    elif temperature == 55:
+        return "It's a little chilly out there!"
+    elif temperature == 99:
+        return "It's too dang hot out there!"
+    elif temperature == 75:
+        return "It's perfect out there!"
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+
+    # acceptable = {
+    #     "+":lambda x,y:x+y,
+    #     "-":lambda x,y:x-y,
+    #     "*":lambda x,y:x*y,
+    #     "/":lambda x,y:x/y,
+    # }
+    
+    # return acceptable.get(operation, lambda x,y:print("Invalid operation!"))(num1,num2)
+
+    if operation == '+':
+        return num1 + num2
+        
+    if operation == '-':
+        return num1 - num2
+        
+    if operation == '*':
+        return num1 * num2
+        
+    if operation == '/':
+        return num1 / num2
+    else:
+        print("Invalid operation!")
+    
+
+
+calculator("+",1,2)
